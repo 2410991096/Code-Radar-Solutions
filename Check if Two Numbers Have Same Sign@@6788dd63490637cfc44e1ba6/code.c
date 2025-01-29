7e3822e92f5ddbaa409bb1b526da9f5964457e83
2410991096 <ekta1096.becse24@chitkara.edu.in>
2025-01-29 18:06:12 +0530

    Solution to Check if Two Numbers Have Same Sign

--- Check if Two Numbers Have Same Sign@@6788dd63490637cfc44e1ba6/code.c
+++ Check if Two Numbers Have Same Sign@@6788dd63490637cfc44e1ba6/code.c
@@ -2,7 +2,7 @@
 
 int main(){
     int  first_num , second_num;
-    scanf("%d%d",&first_num,&second_num)
+    scanf("%d%d",&first_num,&second_num);
     if ((first_num>0)&& (second_num>0)){
         printf("Same Sign");
     }else{
