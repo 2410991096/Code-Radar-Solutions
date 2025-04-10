ff9e4e03cfff282a147ab57f422bb8e15bc98172
Ekta <emangla48@gmail.com>
2025-04-10 13:56:49 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-// Simple Bubble Sort (no function used)
 int main() {
     int n;
     scanf("%d", &n);
+
+    if (n < 2) {
+        // Not enough elements to form a pair
+        printf("-1\n");
+        return 0;
+    }
+
     int arr[n];
 
     // Input elements
@@ -11,11 +17,10 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    // Sort the array (Bubble Sort)
+    // Bubble sort
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -36,13 +41,13 @@ int main() {
         }
     }
 
-   
+    
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
 
     printf("%d %d\n", a, b);
-    
+   
 
     return 0;
 }
